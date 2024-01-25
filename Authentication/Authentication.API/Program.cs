0d98a7bb6d3d3cabcf11c0624969b8c665c8eea4
Mohcine-Stud <mohcinelaaroubi@gmail.com>
2024-01-25 13:33:17 +0000

    api error fixed

--- Authentication/Authentication.API/Program.cs
+++ Authentication/Authentication.API/Program.cs
@@ -1,12 +1,6 @@
-using Authentication.Application;
-using Authentication.Infrastructure;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddApplication();
-builder.Services.AddInfrastructure();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -22,13 +16,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-{
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
-    app.Run();
-}
 
 app.Run();
